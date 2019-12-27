aa4df1a4f1ff9dc91f07198d47b92a19d1d69c25
Greg French <gregfrench26@gmail.com>
2019-12-27 17:08:01 -0500

    Fixed issue #75

--- WheelCurvedPicker.android.js
+++ WheelCurvedPicker.android.js
@@ -29,7 +29,7 @@ const WheelCurvedPickerNative = requireNativeComponent('WheelCurvedPicker', Whee
 
 class WheelCurvedPicker extends React.Component {
 
-	propTypes: {
+	static propTypes = {
 		...View.propTypes,
 
 		data: PropTypes.array,
@@ -97,7 +97,7 @@ class WheelCurvedPicker extends React.Component {
 }
 
 class Item extends React.Component {
-	propTypes: {
+	static propTypes = {
 		value: React.PropTypes.any, // string or integer basically
 		label: React.PropTypes.string,
 	}
