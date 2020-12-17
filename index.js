0af0a66e4e995b2101db1266dbf3aa129ebfb328
GregFrench <gregfrench26@gmail.com>
2020-12-17 18:43:33 -0500

    Moved iOS picker import from index to wheel picker iOS file

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 'use strict';
 
 import React from 'react';
-import {PickerIOS} from '@react-native-picker/picker'
-import {Platform} from 'react-native';
-
 import WheelCurvedPicker from './WheelCurvedPicker'
 
-module.exports = (Platform.OS === 'ios' ? PickerIOS : WheelCurvedPicker)
+module.exports = WheelCurvedPicker
