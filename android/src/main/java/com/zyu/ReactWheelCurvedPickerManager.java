2c580770843aa447cfd1acd1ee29082215d86c15
John Yong <johnyong@live.cn>
2021-01-28 19:15:32 +0800

    [Fix] Set text colour to #777

--- android/src/main/java/com/zyu/ReactWheelCurvedPickerManager.java
+++ android/src/main/java/com/zyu/ReactWheelCurvedPickerManager.java
@@ -69,7 +69,7 @@ public class ReactWheelCurvedPickerManager extends SimpleViewManager<ReactWheelC
     public void setTextColor(ReactWheelCurvedPicker picker, Integer color) {
         if (picker != null) {
             picker.setCurrentTextColor(color);
-            picker.setTextColor(Utils.parseColor("#777"));
+            picker.setTextColor(Color.rgb(120, 120, 120));
         }
     }
 
